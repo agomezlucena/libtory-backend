9cf1b1c25d4e8110cce009d0ebb998ac0950ee60
Alex <aglucena95@gmail.com>
2024-08-13 18:16:07 +0200

    feat: added required NonNull and Nullable annotations in AuthorsId.
    doc: added javadoc in AuthorsId class.

--- libtory-core/src/main/java/io/github/agomezlucena/libtory/books/domain/AuthorsId.java
+++ libtory-core/src/main/java/io/github/agomezlucena/libtory/books/domain/AuthorsId.java
@@ -1,41 +1,72 @@
 package io.github.agomezlucena.libtory.books.domain;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Set;
-import java.util.UUID;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * This record represent a set of authors ids that have a book.
+ * @param ids a set of author ids.
+ * @author Alejandro Gómez Lucena.
+ */
 record AuthorsId(Set<UUID> ids) {
     private static final AuthorsId EMPTY_AUTHORS = new AuthorsId(Collections.emptySet());
 
-    public static AuthorsId from (UUID... authorsId){
-        if(authorsId == null || authorsId.length == 0){
-            return EMPTY_AUTHORS;
-        }
-        return new AuthorsId(Set.of(authorsId));
+    /**
+     * create a new instance with the given authors ids.
+     * @param authorsId authors id
+     * @return an instance with the passed by parameters ids, in case that you pass a null or empty value
+     * will return an empty shared instance.
+     */
+    public static @NonNull AuthorsId from (@Nullable UUID... authorsId){
+        return Optional.ofNullable(authorsId)
+                .filter(it -> it.length > 0)
+                .map(Set::of)
+                .map(AuthorsId::new)
+                .orElse(EMPTY_AUTHORS);
     }
 
-    public AuthorsId addAuthors(UUID... ids){
-        if(ids == null) return this;
-        return new AuthorsId(Stream.concat(
-                this.ids.stream(),
-                Arrays.stream(ids)
-        ).collect(Collectors.toUnmodifiableSet()));
-    }
-
-    public AuthorsId remove(UUID... ids){
-        if(ids == null) return this;
-
-        var authorsToRemove = Set.of(ids);
-
-        if(this.ids.equals(authorsToRemove)){
-            return EMPTY_AUTHORS;
-        }
+    /**
+     * will create a new AuthorId object with the union of the given ids and the id that holds this object.
+     * @param ids given authors id to add
+     * @return <p>if ids is not null and is not empty return the expected value.</p>
+     * <p>if ids are null or empty return the called object.</p>
+     * <p>if called object is empty return a new AuthorId with the input</p>
+     */
+    public @NonNull AuthorsId addAuthors(@Nullable UUID... ids){
+        if(ids == null || ids.length == 0) return this;
+        if(this.ids.isEmpty()) return from(ids);
 
         return new AuthorsId(
-                this.ids.stream().filter(id -> !authorsToRemove.contains(id)).collect(Collectors.toSet())
+                Stream.concat(
+                    this.ids.stream(),
+                    Arrays.stream(ids)
+                ).collect(Collectors.toUnmodifiableSet())
+        );
+    }
+
+    /**
+     * will create a new object without the id passed by parameter
+     * @param ids author id to remove
+     * @return will return a set without the passed ids.
+     * <p>if called object has not id hold will return themself.</p>
+     * <p>if ids are null or empty will return the called object.</p>
+     * <p>if ids are the same that holds the called object will return the shared empty instance.</p>
+     */
+    public @NonNull AuthorsId remove(@Nullable UUID... ids){
+        if(this.ids.isEmpty() || ids == null || ids.length == 0) return this;
+
+        var authorsToRemove = Set.of(ids);
+        if(this.ids.equals(authorsToRemove)) return EMPTY_AUTHORS;
+
+
+        return new AuthorsId(
+                this.ids.stream()
+                        .filter(id -> !authorsToRemove.contains(id))
+                        .collect(Collectors.toSet())
         );
     }
 }
