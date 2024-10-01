a7ea3f2914594a3068e6eae739e3fad66343cfad
Alex <aglucena95@gmail.com>
2024-10-01 12:57:34 +0200

    refactor: reformatted code at AuthorUpdateCommand

--- libtory-books/src/main/java/io/github/agomezlucena/libtory/books/domain/AuthorUpdateCommand.java
+++ libtory-books/src/main/java/io/github/agomezlucena/libtory/books/domain/AuthorUpdateCommand.java
@@ -8,35 +8,38 @@ import java.util.UUID;
 public sealed interface AuthorUpdateCommand {
     /**
      * Will create a command that will add authors to the book.
+     *
      * @param authorChecker will check that the given authors exists.
-     * @param repository persistence representation that contains the book aggregates
-     * @param authorIds an array of uuid that represents the authors to add to the book
+     * @param repository    persistence representation that contains the book aggregates
+     * @param authorIds     an array of uuid that represents the authors to add to the book
      * @return an AuthorCommand that allows to remove authors.
      */
     static AuthorUpdateCommand addAuthors(
             AuthorChecker authorChecker,
             BookRepository repository,
-            UUID...authorIds
-    ){
-        return new AddAuthors(authorChecker,repository,authorIds);
+            UUID... authorIds
+    ) {
+        return new AddAuthors(authorChecker, repository, authorIds);
     }
 
     /**
      * Will create a command that will remove authors from the book.
+     *
      * @param repository persistence representation that contains the book aggregates.
-     * @param authorIds an array of uuid that represents the authors to remove from the book.
+     * @param authorIds  an array of uuid that represents the authors to remove from the book.
      * @return an AuthorCommand that allows to remove authors.
      */
-    static AuthorUpdateCommand deleteAuthors(BookRepository repository, UUID... authorIds){
-        return new DeleteAuthor(repository,authorIds);
+    static AuthorUpdateCommand deleteAuthors(BookRepository repository, UUID... authorIds) {
+        return new DeleteAuthor(repository, authorIds);
     }
 
-    record AddAuthors(AuthorChecker authorChecker, BookRepository repository, UUID...authorIds)
-        implements AuthorUpdateCommand {
+    record AddAuthors(AuthorChecker authorChecker, BookRepository repository, UUID... authorIds)
+            implements AuthorUpdateCommand {
 
     }
 
-    record DeleteAuthor(BookRepository repository, UUID... authorsIds) implements AuthorUpdateCommand {
+    record DeleteAuthor(BookRepository repository, UUID... authorsIds)
+            implements AuthorUpdateCommand {
 
     }
 }
