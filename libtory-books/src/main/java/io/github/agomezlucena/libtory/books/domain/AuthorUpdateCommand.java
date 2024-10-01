8c32bfba92c98d967613b83cd8366377b6315776
Alex <aglucena95@gmail.com>
2024-10-01 11:55:42 +0200

    chore(javadoc): expanded javadoc at AuthorUpdateCommand

--- libtory-books/src/main/java/io/github/agomezlucena/libtory/books/domain/AuthorUpdateCommand.java
+++ libtory-books/src/main/java/io/github/agomezlucena/libtory/books/domain/AuthorUpdateCommand.java
@@ -2,7 +2,17 @@ package io.github.agomezlucena.libtory.books.domain;
 
 import java.util.UUID;
 
+/**
+ * Represent the available action that a user can make with the authors of a book
+ */
 public sealed interface AuthorUpdateCommand {
+    /**
+     * Will create a command that will add authors to the book.
+     * @param authorChecker will check that the given authors exists.
+     * @param repository persistence representation that contains the book aggregates
+     * @param authorIds an array of uuid that represents the authors to add to the book
+     * @return an AuthorCommand that allows to remove authors.
+     */
     static AuthorUpdateCommand addAuthors(
             AuthorChecker authorChecker,
             BookRepository repository,
@@ -11,6 +21,12 @@ public sealed interface AuthorUpdateCommand {
         return new AddAuthors(authorChecker,repository,authorIds);
     }
 
+    /**
+     * Will create a command that will remove authors from the book.
+     * @param repository persistence representation that contains the book aggregates.
+     * @param authorIds an array of uuid that represents the authors to remove from the book.
+     * @return an AuthorCommand that allows to remove authors.
+     */
     static AuthorUpdateCommand deleteAuthors(BookRepository repository, UUID... authorIds){
         return new DeleteAuthor(repository,authorIds);
     }
