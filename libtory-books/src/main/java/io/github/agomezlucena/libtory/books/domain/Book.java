8898580a7767c57156c0baa3c976d3342f629c56
Alex <aglucena95@gmail.com>
2024-10-04 15:43:42 +0200

    feat: implemented equals and hashcode in Book root aggregate.

--- libtory-books/src/main/java/io/github/agomezlucena/libtory/books/domain/Book.java
+++ libtory-books/src/main/java/io/github/agomezlucena/libtory/books/domain/Book.java
@@ -2,6 +2,7 @@ package io.github.agomezlucena.libtory.books.domain;
 
 import org.jspecify.annotations.NonNull;
 
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
@@ -11,6 +12,7 @@ import static io.github.agomezlucena.libtory.books.domain.AuthorUpdateCommand.De
 /**
  * Book class represent a book this is the aggregate root for
  * Book management bounded context.
+ *
  * @author Alejandro Gómez Lucena
  */
 public class Book {
@@ -21,17 +23,18 @@ public class Book {
     /**
      * create a book from inputs ports with the information of the given primitives, and will check
      * the existence of the authors passed in the primitives.
+     *
      * @param primitives an object with the information of the book as primitives.
-     * @param checker an object that check that authors in the primitives objects exists in our systems.
+     * @param checker    an object that check that authors in the primitives objects exists in our systems.
      * @return a book with well-formed isbn, not empty title, and a set of authors if any was passed.
-     * @throws InvalidIsbn if passed ISBN is not well-formed.
-     * @throws InvalidTitle if the passed title is null or blank.
+     * @throws InvalidIsbn   if passed ISBN is not well-formed.
+     * @throws InvalidTitle  if the passed title is null or blank.
      * @throws InvalidAuthor if any of the passed authors is not registered in our systems.
      */
     public static Book createBook(@NonNull BookPrimitives primitives, @NonNull AuthorChecker checker) {
         final var authorIds = primitives.authorsAsArray();
 
-        if(authorIds.length > 0 && !checker.authorsExists(authorIds)) throw new InvalidAuthor();
+        if (authorIds.length > 0 && !checker.authorsExists(authorIds)) throw new InvalidAuthor();
 
         return primitives.toBook();
     }
@@ -52,6 +55,7 @@ public class Book {
 
     /**
      * Will set the title with the value passed by parameter.
+     *
      * @param title a non null or not blank string
      * @throws InvalidTitle when title is null or blank
      */
@@ -61,6 +65,7 @@ public class Book {
 
     /**
      * will return the authors ids of the book.
+     *
      * @return a set with the author id, in case that don't have any will return an empty set.
      */
     public @NonNull Set<UUID> getAuthorsIds() {
@@ -69,14 +74,16 @@ public class Book {
 
     /**
      * will return if the book has authors
+     *
      * @return true when have any false when not.
      */
-    public boolean hasAuthors(){
+    public boolean hasAuthors() {
         return !authorsId.isEmpty();
     }
 
     /**
      * update authors adding or deleting the given authors based in the command past by parameter.
+     *
      * @param command and adding author command or deleting author command.
      * @throws InvalidAuthor if you try to add a not registered author in our systems.
      * @see AuthorUpdateCommand
@@ -84,7 +91,7 @@ public class Book {
     public void updateAuthors(@NonNull AuthorUpdateCommand command) {
         switch (command) {
             case AddAuthors(var checker, var repo, var ids) -> addAuthors(repo, checker, ids);
-            case DeleteAuthor(var repo, var ids) -> deleteAuthors(repo,ids);
+            case DeleteAuthor(var repo, var ids) -> deleteAuthors(repo, ids);
         }
     }
 
@@ -94,14 +101,28 @@ public class Book {
         repository.save(this);
     }
 
-    private void deleteAuthors(BookRepository repository, UUID...authorIds){
+    private void deleteAuthors(BookRepository repository, UUID... authorIds) {
         var currentAuthors = this.authorsId;
         var newAuthors = currentAuthors.remove(authorIds);
-        if(currentAuthors.equals(newAuthors)){
+        if (currentAuthors.equals(newAuthors)) {
             return;
         }
 
         this.authorsId = newAuthors;
         repository.save(this);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book other)) return false;
+
+        return Objects.equals(isbn, other.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn);
+    }
+
 }
