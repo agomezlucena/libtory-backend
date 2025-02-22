692997c2f29c80adddbe78d8ca94889dba87818e
agomezlucena <alejandro_gomez_lucena@hotmail.com>
2025-02-22 18:35:04 +0100

    feat: InvalidAuthor now have a message.

--- libtory-books/src/main/java/io/github/agomezlucena/libtory/books/domain/InvalidAuthor.java
+++ libtory-books/src/main/java/io/github/agomezlucena/libtory/books/domain/InvalidAuthor.java
@@ -1,4 +1,7 @@
 package io.github.agomezlucena.libtory.books.domain;
 
 public class InvalidAuthor extends RuntimeException{
+    InvalidAuthor() {
+        super("At least one of the given authors is not registered in our system.");
+    }
 }
